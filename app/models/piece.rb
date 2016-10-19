adb1289a35ed276f1463d1602b77f6c440825c64
reubenson <reubenson@gmail.com>
2016-10-18 21:23:52 -0400

    update tracklist content, styling, and associated models

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -4,18 +4,21 @@ class Piece < ActiveRecord::Base
   has_many :lineitems, as: :item
   has_many :carts, through: :lineitems
 
+  def duration_in_seconds
+    time = duration_split()
+    return time[:hours] * 60 * 60 + time[:minutes] * 60 + time[:seconds]
+  end
+
   def duration_formatted
-    time = duration.split(":")
-    if time.length == 3
-      hours = time[0]
-      minutes = time[1]
-      seconds = time[2]
+    time = duration_split()
+    hours = time[:hours].to_s
+    minutes = time[:minutes].to_s
+    seconds = time[:seconds].to_s
+    if hours != '0'
       minutes = '0' + minutes if minutes.length == 1
       seconds = '0' + seconds if seconds.length == 1
       [hours,minutes,seconds].join(":")
     else
-      minutes = time[0]
-      seconds = time[1]
       seconds = '0' + seconds if seconds.length == 1
       [minutes,seconds].join(":")
     end
@@ -23,5 +26,25 @@ class Piece < ActiveRecord::Base
 
   def price_string
     '%.2f' % self.price
+  end
+
+  private
+
+  def duration_split
+    time = duration.split(":")
+    if time.length == 3
+      hours = time[0]
+      minutes = time[1]
+      seconds = time[2]
+    else
+      hours = 0
+      minutes = time[0]
+      seconds = time[1]
+    end
+    return {
+      hours: hours.to_i,
+      minutes: minutes.to_i,
+      seconds: seconds.to_i
+    }
   end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -7,4 +7,40 @@ class Work < ActiveRecord::Base
 
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  def duration_in_seconds
+    pieces.inject(0) { |sum,piece| sum + piece.duration_in_seconds() }
+  end
+
+  def duration_formatted
+    duration = duration_in_seconds()
+    hours = 0
+    minutes = 0
+    seconds = 0
+    while duration > 3600
+      duration -= 3600
+      hours += 1
+    end
+    while duration > 60
+      duration -= 60
+      minutes += 1
+    end
+    seconds = duration
+    hours = hours.to_s
+    minutes = minutes.to_s
+    seconds = seconds.to_s
+    if hours != '0'
+      minutes = '0' + minutes if minutes.length == 1
+      seconds = '0' + seconds if seconds.length == 1
+      [hours,minutes,seconds].join(":")
+    else
+      seconds = '0' + seconds if seconds.length == 1
+      [minutes,seconds].join(":")
+    end
+  end
+
+  def price_formatted
+    '%.2f' % self.price
+  end
+
 end
